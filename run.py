84a56d432b2a1513e577549b14df7e42558bfc5c
family <bryan951112@gmail.com>
2017-11-16 10:22:53 -0500

    save

--- run.py
+++ run.py
@@ -181,4 +181,4 @@ def satisfaction():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=12345, use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, port=12345, use_reloader=True)
